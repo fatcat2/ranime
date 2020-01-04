c9237b0acd70a7aea0d3ab669e7ad187409ef84a
Ryan Chen <ryanjchen2@gmail.com>
2020-01-04 14:21:52 -0800

    Added image and air date

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import random
 from quart import Quart, render_template, send_from_directory, jsonify
 import httpx
 
-app = Quart(__name__, static_folder="build/static", template_folder="build")
+app = Quart(__name__, static_folder="ranime-client/build/static", template_folder="ranime-client/build")
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -21,17 +21,23 @@ async def random_anime():
     got_result = False
     while not got_result:
         random_id: int = random.randint(1, 10000)
-        print(random_id)
+        # print(random_id)
         query = '''
         query ($id: Int) { # Define which variables will be used in the query (id)
         Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
             id
             title {
-            romaji
-            english
-            native
+                romaji
+                english
+                native
             }
             genres
+            description
+            coverImage {
+                extraLarge
+            }
+            season
+            seasonYear
         }
         }
         '''
@@ -41,15 +47,24 @@ async def random_anime():
         }
 
         url = 'https://graphql.anilist.co'
-
         response = await httpx.post(url, json={'query': query, 'variables': variables})
         result = json.loads(response.text)
+        ret_dict = {}
         if result["data"]["Media"] != None:
+            # print(result)
             got_result == True
-            r = result["data"]["Media"]["title"]["english"]
-            if r == None:
-                r = result["data"]["Media"]["title"]["romaji"]
-            return jsonify(r)
+            ret_dict["title"] = result["data"]["Media"]["title"]["english"]
+            if ret_dict["title"] == None:
+                ret_dict["title"] = result["data"]["Media"]["title"]["romaji"]
+            ret_dict["genres"] = result["data"]["Media"]["genres"]
+            ret_dict["image"] = result["data"]["Media"]["coverImage"]["extraLarge"]
+            ret_dict["description"] = result["data"]["Media"]["description"]
+            try:
+                ret_dict["aired"] = f"Aired {result['data']['Media']['season'].lower().capitalize()} {result['data']['Media']['seasonYear']}"
+            except:
+                ret_dict["aired"] = ""
+            # print(ret_dict)
+            return jsonify(ret_dict)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- ranime-client/src/App.js
+++ ranime-client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Container, Header, Grid, Button } from 'semantic-ui-react'
+import { Card, Image, Icon, Container, Header, Grid, Button, Item } from 'semantic-ui-react'
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,32 +16,52 @@ class RandomAnime extends React.Component {
     this.state = {
       "progress": 0,
       "name": "",
-      "anime": "",
-      "tags": [],
+      "title": "Loading...",
+      "genres": [],
+      "image": "https://media1.giphy.com/media/AAmhvrZzLCz1m/giphy.gif",
+      "description": "いづれの御時にか、女御、更衣あまたさぶらひたまひけるなかに、いとやむごとなき際にはあらぬが、すぐれて時めきたまふありけり。はじめより我はと思ひ上がりたまへる御方がた、めざましきものにおとしめ嫉みたまふ。同じほど、それより下臈の更衣たちは、ましてやすからず。朝夕の宮仕へにつけても、人の心をのみ動かし、恨みを負ふ積りにやありけむ、いとあつしくなりゆき、もの心細げに里がちなるを、いよいよあかずあはれなるものに思ほして、人の そしりをもえ憚らせたまはず、世のためしにもなりぬべき御もてなしなり。",
+      "aired": "Aired in Spring 2020"
     }
   }
 
   componentDidMount() {
     axios.get(`/data/anime`)
       .then(res => {
-        const anime = res.data;
-        this.setState({ anime });
+        const title = res.data.title;
+        const genres = res.data.genres;
+        const image = res.data.image
+        const description = res.data.description
+        const aired = res.data.aired
+        this.setState({ title });
+        this.setState({ genres });
+        this.setState({ image });
+        this.setState({ description });
+        this.setState({ aired });
       })
   }
 
   render(){
     return (
-        <Grid centered columns={1}>
+      <div class="App-card">
+        <Grid centered columns={2}>
             <Grid.Column>
-              <div class="App-header">
-                <Container>
-                  <Header as="h1">
-                    { this.state.anime }
-                  </Header>
-                </Container>
-              </div>
+                  <Image src={this.state.image} />
+            </Grid.Column>
+            <Grid.Column>
+              <Card>
+              <Card.Content>
+                <Card.Header>{this.state.title}</Card.Header>
+                <Card.Meta>
+                  <span className='date'>{this.state.aired}</span>
+                </Card.Meta>
+                <Card.Description>
+                  {this.state.description}
+                </Card.Description>
+              </Card.Content>
+              </Card>
             </Grid.Column>
         </Grid>
+        </div>
       );
     }
 }
